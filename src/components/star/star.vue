8c468486728511cbd7ef602a3213ba00bc8cf305
Chen Simin <375021462@qq.com>
2017-12-27 20:28:03 +0800

    2017.12.27 1.header组件剩浮层 2.star组件写完 3.添加问题记录

---
+++ src/components/star/star.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <input>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+        </ul>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: '',
+    components: {},
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
