ce361cb83c3d453d70d6ca7497c86a15acb36448
Chen Simin <375021462@qq.com>
2017-12-30 18:01:09 +0800

    完成foods部分组件及样式

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,14 +3,39 @@
       <div class="menu-wrapper">
         <ul>
           <li class="menu-item" v-for="item in goods">
-            <span class="text border-1px" >
+            <span class="text" >
               <span class="icon" v-show="item.type > 0" :class="classMap[item.type]"></span>
               {{item.name}}
             </span>
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper"></div>
+      <div class="foods-wrapper">
+        <ul>
+          <li class="food-list" v-for="item in goods">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li class="food-item" v-for="food in item.foods">
+                <div class="icon">
+                  <img :src="food.icon" width="57px" height="57px">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span>月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span><span>￥</span>{{food.price}}</span>
+                    <span class="old-price" v-show="food.oldPrice"><span>￥</span>{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
