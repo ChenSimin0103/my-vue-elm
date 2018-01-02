de573f19e75b2096eb0b0e89c039eb8e80e85197
Chen Simin <375021462@qq.com>
2018-01-02 18:44:33 +0800

    2018.1.2 goods组件做出滑动效果及对应菜单子元素背景改变

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="goods">
-      <div class="menu-wrapper">
+      <div class="menu-wrapper" ref="menuWrapper">
         <ul>
-          <li class="menu-item" v-for="item in goods">
+          <li v-for="(item,index) in goods"
+              class="menu-item"
+              :class="{'current':currentIndex===(index)}"
+              @click="selectMenu(index,$event)"
+              ref="menuList" >
             <span class="text" >
               <span class="icon" v-show="item.type > 0" :class="classMap[item.type]"></span>
               {{item.name}}
@@ -10,9 +14,9 @@
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper">
+      <div class="foods-wrapper" ref="foodsWrapper">
         <ul>
-          <li class="food-list" v-for="item in goods">
+          <li class="food-list" ref="foodList" v-for="item in goods">
             <h1 class="title">{{item.name}}</h1>
             <ul>
               <li class="food-item" v-for="food in item.foods">
@@ -23,12 +27,12 @@
                   <h2 class="name">{{food.name}}</h2>
                   <p class="desc">{{food.description}}</p>
                   <div class="extra">
-                    <span>月售{{food.sellCount}}份</span>
+                    <span class="extra-sell">月售{{food.sellCount}}份</span>
                     <span>好评率{{food.rating}}%</span>
                   </div>
                   <div class="price">
-                    <span><span>￥</span>{{food.price}}</span>
-                    <span class="old-price" v-show="food.oldPrice"><span>￥</span>{{food.oldPrice}}</span>
+                    <span class="new-price"><span class="price-icon">￥</span>{{food.price}}</span>
+                    <span class="old-price" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                   </div>
                 </div>
               </li>
@@ -40,6 +44,9 @@
 </template>
 
 <script type="text/ecmascript-6">
+
+  import BScroll from 'better-scroll'
+
   const ERR_OK = 0
 
   export default {
@@ -52,10 +59,62 @@
     components: {},
     data () {
       return {
-        goods: {}
+        goods: {},
+        listHeight: [],
+        scrollY: 0
+      }
+    },
+    computed: {
+      currentIndex() {
+        // 用来测试问题
+//        let num = Math.round(this.scrollY/400)
+//        return num
+        for (let i=0;i<this.listHeight.length;i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i+1]
+          if (!height2 || (this.scrollY >= height1 && this.scrollY <= height2)) {
+            return i
+            // console.log('当前菜单落在：' + i)
+          }
+        }
+        return -1
       }
     },
     methods: {
+      selectMenu(index,event) {
+        // console.log(index)
+        if (!event._constructed) {
+          return
+        }
+      },
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper,{
+          // 初始化BScroll时给click属性才能实现移动设备点击（其实是滑动触摸），否则只能响应原生点击事件
+          click: true
+        })
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper,{
+          click: true,
+          probeType: 3
+        })
+        this.foodsScroll.on('scroll',(pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y))
+          // 此处需要先取整在变正数
+          // this.scrollY = Math.round(pos.y)
+          // this.scrollY = pos.y
+          console.log(this.scrollY)
+        })
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodList
+        let height = 0
+        this.listHeight.push(height);
+        for (let i = 0; i<foodList.length; i++){
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+          console.log('foodList高度是：' + this.listHeight)
+        }
+      }
 
     },
     created() {
@@ -64,7 +123,13 @@
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
-          console.log(this.goods)
+          // 不使用 $nextTick()，this._initScroll()正常，this._calculateHeight()显示未定义的对象
+          // this._initScroll();
+          this.$nextTick(() => {
+            this._initScroll()
+            this._calculateHeight()
+          })
+          // console.log(this.goods)
         }
       })
         // 因为传入了seller,其中的classMap还需再定义一次
